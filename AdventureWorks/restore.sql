018da1ad1dc23b403b8a42d8db9a32b9d5984847
류경석 <narzis@gmail.com>
2022-07-06 10:16:08 +0900

    Add project files.

---
+++ AdventureWorks/restore.sql
@@ -0,0 +1,3 @@
+﻿RESTORE DATABASE AdventureWorks FROM DISK='C:\Users\wtime\Downloads\AdventureWorksLT2019.bak'
+WITH MOVE 'AdventureWorksLT2012_Data' TO 'C:\Users\wtime\Downloads\AdventureWorksLT2019.mdf',
+MOVE 'AdventureWorksLT2012_Log' TO 'C:\Users\wtime\Downloads\AdventureWorksLT2019.ldf'
