2d4dd7c5e7dba3efeabec0291f38a54abaf8d586
류경석 <narzis@gmail.com>
2022-07-06 12:31:14 +0900

    format

--- AdventureWorks/Controllers/CustomersController.cs
+++ AdventureWorks/Controllers/CustomersController.cs
@@ -17,11 +17,12 @@ namespace AdventureWorks.Controllers
         }
 
         [HttpGet("search-by-firstname")]
-        public async Task<ActionResult<IEnumerable<CustomerDTO>>> GetCusotmers([FromQuery]string firstName)
-        { 
+        public async Task<ActionResult<IEnumerable<CustomerDTO>>> GetCusotmers([FromQuery] string firstName)
+        {
             var customers = await _context.Customers
                 .Where(x => x.FirstName == firstName)
-                .Select(x => new CustomerDTO {
+                .Select(x => new CustomerDTO
+                {
                     Id = x.CustomerId,
                     FirstName = x.FirstName,
                     LastName = x.LastName
@@ -30,26 +31,28 @@ namespace AdventureWorks.Controllers
         }
 
         [HttpGet("vips")]
-        public async Task<IActionResult> GetVips([FromQuery]int averageTotalDue)
+        public async Task<IActionResult> GetVips([FromQuery] int averageTotalDue)
         {
-            var customers = await _context.Customers.Where(c => c.SalesOrderHeaders
-               .GroupBy(g => g.CustomerId,
-               (k, v) =>
-               new
-               {
-                   CustomerId = k,
-                   AverageTotalDue = v.Average(x => x.TotalDue)
-               }).Any(x => x.AverageTotalDue > averageTotalDue)
+            var customers = await _context.Customers
+               .Where(c => c.SalesOrderHeaders
+                .GroupBy(g => g.CustomerId,
+                   (k, v) =>
+                   new
+                   {
+                       CustomerId = k,
+                       AverageTotalDue = v.Average(x => x.TotalDue)
+                   }).Any(x => x.AverageTotalDue > averageTotalDue)
                )
-               .Select(c => new VIPItemDTO {
-                CustomerId = c.CustomerId,
-                CustomerName = c.FirstName + " " + c.LastName,
-                AverageTotalDue = c.SalesOrderHeaders.Average(x => x.TotalDue)
+               .Select(c => new VIPItemDTO
+               {
+                   CustomerId = c.CustomerId,
+                   CustomerName = c.FirstName + " " + c.LastName,
+                   AverageTotalDue = c.SalesOrderHeaders.Average(x => x.TotalDue)
                })
                .OrderByDescending(x => x.AverageTotalDue)
                .Take(5)
                .ToListAsync();
-                
+
 
 
             //var q = await _context.SalesOrderHeaders
